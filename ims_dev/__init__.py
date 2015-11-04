9d0e09d56fe364b219d7d9c01cbab7222817f279
Rob <grovesr1@yahoo.com>
2015-11-04 07:22:35 -0500

    feat: add date and comments to version information

--- ims_dev/__init__.py
+++ ims_dev/__init__.py
@@ -1,1 +1,5 @@
-VERSION='1.3.0'
+# version will be used to tag release in GIT
+# date format = dd/mm/YYYY
+# comment will be used to add comment to GIT tag
+#VERSION=('1.3.0','02/11/2015','add version, admin. server settings')
+VERSION=('1.4.0','10/11/2015','allow auto generation of product codes. Add date and comments to version')
